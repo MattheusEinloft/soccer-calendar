e2bae1415656cdfb782f231b33a0a2f3f9329ba7
Mattheus Einloft <mattheuseinloft@gmail.com>
2019-10-06 16:19:12 -0300

    refactor: put input on another file

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
-const readline = require('readline-sync');
+const input = require('./input');
 
 function start() {
     const content = {};
 
-    content.searchTerm = askAndReturnSearchTerm();
-
-    function askAndReturnSearchTerm() {
-        return readline.question('Informe o time que voce gostaria de adicionar no Calendar: ');
-    }
+    content.searchTerm = input;
 
     console.log(content);
 }
